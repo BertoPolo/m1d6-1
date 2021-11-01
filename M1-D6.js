f599efb0509f5acb558ab8f6d37b232be214e9e1
BertoPolo <bertopolo91@gmail.com>
2021-11-01 11:33:06 +0100

    Update M1-D6.js

--- M1-D6.js
+++ M1-D6.js
@@ -14,26 +14,40 @@ ASSIGNMENT RULES
 /* EXERCISE A
    Create a variable called test and assign a string value to it.
 */
+let test = "this is a test"
 
 /* EXERCISE B
     Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
 */
 
+let sum = (10+20)
+console.log(sum)
+
 /* EXERCISE C
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
+
 
 /* EXERCISE D
     Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
 
+let me= {
+  name: "Alberto",
+  surname: "Polo",
+  age: 30
+}
+
 /* EXERCISE E
     Write a piece of code for programmatically removing the age property from the previously create object.
 */
+delete me.age
+console.log(me)
 
 /* EXERCISE F
    Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
 */
+me.skills = ["javascript","html","css"]a
 
 /* EXERCISE G
    Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
